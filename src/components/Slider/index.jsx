eaa2e51df7f64aae1b65482e074550d19917a403
Вячеслав <kozub.vm@gmail.com>
2024-01-25 23:18:48 +0100

    add button autoplay

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -6,27 +6,73 @@ import Slide from '../Slide';
 
 class Slider extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      index: 0,
+      autoPlay: false
+    };
+  }
+  handleClickNext = () => {
+    // const { index } = this.state; якщо зробити деструкторизацію, то this.state.index= index
+    if (this.state.index === this.props.slides.length -1) {
+      this.setState({
+        index: 0,
+      }); 
+      return
+    }
   
-  
-  
-//   handleDelete = (id) => {
+    this.setState({
+      index: this.state.index + 1,
+    });
+  };
+
+  handleClickPrev = () => {
     
-//     const newUsersArr = this.state.usersArr.filter((user) => id !== user.id  )
-//     this.setState({usersArr: newUsersArr})
-//     console.log(newUsersArr);
-//   };
-  
+    if (this.state.index === 0) {
+      this.setState({
+        index: this.props.slides.length - 1,
+      }); 
+      return
+    }
+    
+
+    this.setState({
+      index: this.state.index - 1,
+    });
+  };
+
+  handleAutoPlay = () => {
+    this.setState((prevState) => ({
+      autoPlay: !prevState.autoPlay
+    }))
+    
+  }
+  componentDidMount(){
+    
+        this.intervalId = setInterval(() => {
+          if (this.state.autoPlay) {
+            this.handleClickNext()
+          }
+        }, 1000);
+      
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
   render() {
     const {slides} = this.props
-    console.log(slides[1].image);
-
-    
     
     return (
       <>
-        <Slide image={slides[0].image} description={slides[0].description}/>
-        <button>Previous</button>
-        <button>Next</button>
+        <Slide image={slides[this.state.index].image} description={slides[this.state.index].description}/>
+        <button onClick={this.handleClickPrev}>Previous</button>
+        <button onClick={this.handleAutoPlay}>{this.state.autoPlay ? "Stop" : "Play" }</button>
+        <button onClick={this.handleClickNext}>Next</button>
+        <input></input>
 
       </>
     )
